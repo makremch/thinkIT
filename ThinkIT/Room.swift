3120626fb46e0cb24fcdf6931ccfe8e44487a3ff
makremch <makrem.hamani@esprit.tn>
2020-12-30 14:53:46 +0100

    Room class

---
+++ ThinkIT/Room.swift
@@ -0,0 +1,29 @@
+//
+//  Room.swift
+//  ThinkIT
+//
+//  Created by Makrem chambah on 30/12/2020.
+//
+
+import Foundation
+import UIKit
+
+
+
+class Room {
+    public var image : UIImage
+    public var name : String = ""
+    public var numberOfDevices : Int = 0
+    
+    init(image : UIImage, name: String, numberOfDevices : Int) {
+        self.image = image
+        self.name = name
+        self.numberOfDevices = numberOfDevices
+    }
+    
+    
+    
+    
+    
+    
+}
